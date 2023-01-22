748348bfb0858df5022aca5dd9e45e498fd319dc
Jamie Kennedy <jamiekennedy97@gmail.com>
2023-01-22 15:48:04 +0000

    Initial project setup with vite and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+
+function App() {
+    return (
+        <div>
+            <p className="font-bold">Hello World</p>
+        </div>
+    );
+}
+
+export default App;
